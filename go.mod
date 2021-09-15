a874611f7af8640a3c6b341855b247c108f78684
ozbe <1372945+ozbe@users.noreply.github.com>
2021-09-15 21:44:57 +1000

    implement flow to acquire Spotify token using the authorization code flow

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/ozbe/spotio
+
+go 1.16
+
+require (
+	github.com/google/uuid v1.3.0
+	github.com/joho/godotenv v1.3.0
+	github.com/zmb3/spotify/v2 v2.0.0
+)
---
+++ main.go
@@ -0,0 +1,65 @@
+// Seeded from https://github.com/zmb3/spotify/blob/master/examples/authenticate/authcode/authenticate.go
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/zmb3/spotify/v2"
+	spotifyauth "github.com/zmb3/spotify/v2/auth"
+	"golang.org/x/oauth2"
+
+	"github.com/google/uuid"
+)
+
+func main() {
+	const redirectURI = "http://localhost:8080/callback"
+	auth := spotifyauth.New(
+		spotifyauth.WithRedirectURL(redirectURI),
+		spotifyauth.WithScopes(spotifyauth.ScopeUserReadCurrentlyPlaying, spotifyauth.ScopeUserReadPlaybackState, spotifyauth.ScopeUserModifyPlaybackState),
+	)
+	ch := make(chan *oauth2.Token)
+	state := uuid.NewString()
+
+	http.Handle("/callback", completeAuth(auth, state, ch))
+
+	go func() {
+		if err := http.ListenAndServe(":8080", nil); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	url := auth.AuthURL(state)
+	fmt.Println("Follow auth url to generate token:", url)
+
+	tok := <-ch
+	fmt.Printf("Token: %#v\n", tok)
+
+	ctx := context.Background()
+	client := spotify.New(auth.Client(ctx, tok))
+	user, err := client.CurrentUser(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("User ID:", user.ID)
+}
+
+func completeAuth(auth *spotifyauth.Authenticator, state string, ch chan<- *oauth2.Token) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		tok, err := auth.Token(r.Context(), state, r)
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
+			log.Fatal("Couldn't exchange token", err)
+		}
+		if st := r.FormValue("state"); st != state {
+			http.NotFound(w, r)
+			log.Fatalf("State mismatch: %s != %s\n", st, state)
+		}
+		fmt.Fprint(w, "Login complete")
+		ch <- tok
+	})
+}
